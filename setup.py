1dbedc4c2c62fc2010ae202a00ee9f3b00c186a6
marzwu <marzwu@gmail.com>
2026-09-07 15:43:47 +0800

    ! update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='pybit',
-    version=__version__,
+    version='5.17.2',
     description='Python3 Bybit HTTP/WebSocket API Connector', 
     long_description=long_description,
     long_description_content_type="text/markdown",
